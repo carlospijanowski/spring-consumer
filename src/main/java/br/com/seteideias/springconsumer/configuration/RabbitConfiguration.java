b964e41e7b470d6a0df5a463cb5fd3147f6baafc
Carlos <carlospc1978@gmail.com>
2022-01-02 15:10:35 -0300

    Initial commit

---
+++ src/main/java/br/com/seteideias/springconsumer/configuration/RabbitConfiguration.java
@@ -0,0 +1,10 @@
+package br.com.seteideias.springconsumer.configuration;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitConfiguration {
+
+
+
+}
---
+++ src/main/java/br/com/seteideias/springconsumer/dto/Message.java
@@ -0,0 +1,10 @@
+package br.com.seteideias.springconsumer.dto;
+
+import lombok.Data;
+
+@Data
+public class Message {
+
+    private String text;
+
+}
