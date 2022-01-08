82c0aff2c18283cebeb27a17a667e41fd37a68dd
Carlos <carlospc1978@gmail.com>
2022-01-08 11:31:45 -0300

    documentacao swagger

--- src/main/java/br/com/seteideias/springconsumer/SpringConsumerApplication.java
+++ src/main/java/br/com/seteideias/springconsumer/SpringConsumerApplication.java
@@ -1,13 +1,26 @@
 package br.com.seteideias.springconsumer;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@Api(value = "API REST Consumidora")
 @SpringBootApplication
+@RestController
+@RequestMapping(value = "/")
 public class SpringConsumerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringConsumerApplication.class, args);
+    }
+
+    @ApiOperation(value = "apenas uma mensagem de retorno")
+    @RequestMapping(value = "/")
+    public String endPointDeTest(){
+        return "apenas um retorno de teste";
     }
 
 }
