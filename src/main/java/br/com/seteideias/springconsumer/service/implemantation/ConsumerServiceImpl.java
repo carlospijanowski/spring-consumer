a8b423da63ff396e23e2a939ee6564bb61cf11c4
Carlos Pijanowski Cartaxo <carlospc1978@gmail.com>
2022-11-08 20:34:54 -0300

    implementacao do elastic e kibana no docker-compose.yml

--- src/main/java/br/com/seteideias/springconsumer/service/implemantation/ConsumerServiceImpl.java
+++ src/main/java/br/com/seteideias/springconsumer/service/implemantation/ConsumerServiceImpl.java
@@ -4,7 +4,6 @@ import br.com.seteideias.springconsumer.dto.Message;
 import br.com.seteideias.springconsumer.service.ConsumerService;
 import org.springframework.stereotype.Service;
 
-import javax.swing.*;
 import java.util.Date;
 
 @Service
@@ -12,10 +11,10 @@ public class ConsumerServiceImpl implements ConsumerService {
 
     @Override
     public void acaoObrigatoriaDeQuandoRealizarOConsumer(Message message) {
-        Message message1 = new Message(message.getText(),message.getText2(),new Date());
-        System.err.printf(message1.getText()+"\n");
-        System.err.printf(message1.getText2()+"\n");
-        System.err.printf(message1.getDate()+"\n");
+        Message message1 = new Message(message.getText(), message.getText2(), new Date());
+        System.err.printf(message1.getText() + "\n");
+        System.err.printf(message1.getText2() + "\n");
+        System.err.printf(message1.getDate() + "\n");
     }
 
 }
